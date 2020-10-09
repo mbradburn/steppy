4d0ba3952d83315b8b7061c89169992134b968e0
Matthew Bradburn <bradburn.m@gmail.com>
2020-10-09 11:45:13 -0700

    update

--- src/Function/index.js
+++ src/Function/index.js
@@ -4,15 +4,5 @@ exports.handler = async (event, context) => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
 
-  console.log(process.env.STATE_MACHINE_ARN);
-
-  const stepFunctions = new AWS.StepFunctions();
-  const result = await stepFunctions.startExecution({
-    stateMachineArn: process.env.STATE_MACHINE_ARN,
-    input: '{ }'
-  }).promise()
-
-  console.log('Result: ', result);
-
   return {};
 };
