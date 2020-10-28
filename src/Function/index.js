e2a267c4077bea9017b2f5a88d6de5ba1a0ad4df
Matthew Bradburn <matthew+test@stackery.io>
2020-10-28 09:51:22 -0700

    Modified 3 resources:
    Updated table resource asdf
    Updated facet resource StateMachineFacettaskResourceStateIdTaskState
    Deleted function resource asdf

--- src/Function/index.js
+++
@@ -1,8 +0,0 @@
-const AWS = require('aws-sdk');
-
-exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
-
-  return {};
-};
